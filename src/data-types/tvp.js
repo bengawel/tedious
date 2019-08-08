ff17a035774caed697cda9a8ff3e9c26759b5b5f
Ben Gawel <bengawel@gmail.com>
2019-08-07 21:00:34 -0700

    feat(buffer allocation): Added getParameterBufferLength and getTypeInfoBufferLength methods to tvp type
    Changed how writeParameterData method works for tvp type. Call getParamterBufferLength method to get
    required buffer length and make a new tempBuffer.  Write all parameter data to the temp buffer
    and finally write the tempBuffer to the buffer passed into the writeParameterData method.

--- src/data-types/tvp.js
+++ src/data-types/tvp.js
@@ -1,3 +1,5 @@
+const WritableTrackingBuffer = require('./tracking-buffer/writable-tracking-buffer');
+
 module.exports = {
   id: 0xF3,
   type: 'TVPTYPE',
@@ -5,6 +7,54 @@ module.exports = {
 
   declaration: function(parameter) {
     return parameter.value.name + ' readonly';
+  },
+
+  getParameterDataBufferLength: function(parameter, options) {
+    if (parameter.value === null) {
+      return WritableTrackingBuffer.getUInt16LELength() + WritableTrackingBuffer.getUInt8Length() + WritableTrackingBuffer.getUInt8Length();
+    }
+    let length = 0;
+    length += WritableTrackingBuffer.getUInt16LELength();
+    const ref = parameter.value.columns;
+    for (let i = 0, len = ref.length; i < len; i++) {
+      const column = ref[i];
+      length += WritableTrackingBuffer.getUInt32LELength();
+      length += WritableTrackingBuffer.getUInt16LELength();
+      length += column.type.getTypeInfoBufferLength(column);
+      length += WritableTrackingBuffer.getBVarcharLength('');
+    }
+
+    length += WritableTrackingBuffer.getUInt8Length();
+
+    const ref1 = parameter.value.rows;
+    for (let j = 0, len1 = ref1.length; j < len1; j++) {
+      const row = ref1[j];
+
+      length += WritableTrackingBuffer.getUInt8Length();
+
+      for (let k = 0, len2 = row.length; k < len2; k++) {
+        const value = row[k];
+        const param = {
+          value: value,
+          length: parameter.value.columns[k].length,
+          scale: parameter.value.columns[k].scale,
+          precision: parameter.value.columns[k].precision
+        };
+        length += parameter.value.columns[k].type.getParameterDataBufferLength(param, options);
+      }
+    }
+
+    length += WritableTrackingBuffer.getUInt8Length();
+
+    return length;
+  },
+
+  getTypeInfoBufferLength(parameter) {
+    let ref, ref1, ref2, ref3;
+    return WritableTrackingBuffer.getUInt8Length() + 
+      WritableTrackingBuffer.getBVarCharLength('') + 
+      WritableTrackingBuffer.getBVarCharLength((ref = (ref1 = parameter.value) != null ? ref1.schema : undefined) != null ? ref : '') +
+      WritableTrackingBuffer.getBVarCharLength((ref2 = (ref3 = parameter.value) != null ? ref3.name : undefined) != null ? ref2 : '');
   },
 
   writeTypeInfo: function(buffer, parameter) {
@@ -23,29 +73,28 @@ module.exports = {
       return;
     }
 
-    buffer.writeUInt16LE(parameter.value.columns.length);
+    const requiredLength = getParameterDataBufferLength(parameter, options);
+
+    let tempBuffer = new WritableTrackingBuffer(requiredLength);
+
+    tempBuffer.writeUInt16LE(parameter.value.columns.length);
 
     const ref = parameter.value.columns;
     for (let i = 0, len = ref.length; i < len; i++) {
       const column = ref[i];
-      buffer.writeUInt32LE(0x00000000);
-      buffer.writeUInt16LE(0x0000);
-      column.type.writeTypeInfo(buffer, column);
-      buffer.writeBVarchar('');
+      tempBuffer.writeUInt32LE(0x00000000);
+      tempBuffer.writeUInt16LE(0x0000);
+      column.type.writeTypeInfo(tempBuffer, column);
+      tempBuffer.writeBVarchar('');
     }
 
-    buffer.writeUInt8(0x00);
+    tempBuffer.writeUInt8(0x00);
 
     const ref1 = parameter.value.rows;
-    const writeNext = (i) => {
-      if (i >= ref1.length) {
-        buffer.writeUInt8(0x00);
-        cb();
-        return;
-      }
-      const row = ref1[i];
+    for (let j = 0, len1 = ref1.length; j < len1; j++) {
+      const row = ref1[j];
 
-      buffer.writeUInt8(0x01);
+      tempBuffer.writeUInt8(0x01);
 
       for (let k = 0, len2 = row.length; k < len2; k++) {
         const value = row[k];
@@ -55,15 +104,13 @@ module.exports = {
           scale: parameter.value.columns[k].scale,
           precision: parameter.value.columns[k].precision
         };
-        parameter.value.columns[k].type.writeParameterData(buffer, param, options, () => {});
+        parameter.value.columns[k].type.writeParameterData(tempBuffer, param, options);
       }
+    }
 
-      setImmediate(() => {
-        writeNext(i + 1);
-      });
-    };
+    tempBuffer.writeUInt8(0x00);
 
-    writeNext(0);
+    buffer.writeBuffer(tempBuffer);
   },
   validate: function(value) {
     if (value == null) {
